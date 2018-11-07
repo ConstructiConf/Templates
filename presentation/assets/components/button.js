68534f4945523c6c7e2982e90d236a22a6d88309
Raymond Hill <infamous.rj@gmail.com>
2018-11-06 22:00:37 -0500

    Added a link on theming, tweaked the button. [WIP]

--- presentation/assets/components/button.js
+++ presentation/assets/components/button.js
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 const SetupButton = styled.a`
-  font-family: "Roboto", sans-serif;
-  font-size: 42px;
-  text-transform: uppercase;
-  letter-spacing: 2.5px;
-  font-weight: 500;
-  color: #888;
   background-color: #ddd;
   border: none;
   border-radius: 5px;
-  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease 0s;
-  text-decoration: none;
-  padding: 16px;
+  box-shadow: 0px 8px 15px rgba(191, 191, 191, 0.4);
+  color: #888;
+  font-family: "Roboto", sans-serif;
+  font-size: 42px;
+  font-weight: 500;
+  letter-spacing: 2.5px;
   outline: none;
+  padding: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  transition: all 0.3s ease 0s;
 
   &:hover {
     background-color: #d3d;
-    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.4);
+    box-shadow: 0px 8px 15px rgba(240, 50, 240, 0.4);
     color: #000;
-    transform: translateY(-7px);
+    transform: translateY(-8px);
   }
 `;
 
